16dedce6b86cb0f71c0db7b0ce6b385cd1d45817
Anthony Mercado <mercado.anthony.e@gmail.com>
2023-04-30 23:19:12 -0400

    Add Neows Fallen Event

--- src/main/java/PrismaticEvents/PrismaticEventsMod.java
+++ src/main/java/PrismaticEvents/PrismaticEventsMod.java
@@ -3,17 +3,28 @@ package PrismaticEvents;
 import basemod.BaseMod;
 import basemod.interfaces.*;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.utils.compression.lzma.Base;
 import com.evacipated.cardcrawl.mod.stslib.Keyword;
 import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer;
 import com.google.gson.Gson;
+import com.megacrit.cardcrawl.dungeons.Exordium;
+import com.megacrit.cardcrawl.localization.EventStrings;
+
+import PrismaticEvents.events.NeowsFallenEvent;
 
 import java.nio.charset.StandardCharsets;
 
 @SuppressWarnings({"unused", "WeakerAccess"})
 @SpireInitializer
-public class PrismaticEventsMod implements PostInitializeSubscriber {
+public class PrismaticEventsMod implements
+    PostInitializeSubscriber,
+    EditStringsSubscriber {
 
-    public static final String modID = "launchpad"; //TODO: Change this.
+    public static final String modID = "prismaticEvents";
+
+    public static String makeEventPath(String resourcePath) {
+        return modID + "Resources/images/events/" + resourcePath;
+    }
 
     public static String makeID(String idText) {
         return modID + ":" + idText;
@@ -37,6 +48,12 @@ public class PrismaticEventsMod implements PostInitializeSubscriber {
 
     @Override
     public void receivePostInitialize() {
+        BaseMod.addEvent(NeowsFallenEvent.ID, NeowsFallenEvent.class, Exordium.ID);
+    }
 
+    @Override
+    public void receiveEditStrings() {
+        BaseMod.loadCustomStringsFile(EventStrings.class,
+                modID + "Resources/localization/eng/Event-Strings.json");
     }
 }
--- src/main/java/PrismaticEvents/events/NeowsFallenEvent.java
+++ src/main/java/PrismaticEvents/events/NeowsFallenEvent.java
@@ -1,20 +1,106 @@
 package PrismaticEvents.events;
 
+import java.util.ArrayList;
+
+import static PrismaticEvents.PrismaticEventsMod.makeEventPath;
+
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.events.AbstractImageEvent;
+import com.megacrit.cardcrawl.helpers.RelicLibrary;
+import com.megacrit.cardcrawl.localization.EventStrings;
+import com.megacrit.cardcrawl.relics.AbstractRelic;
+import com.megacrit.cardcrawl.relics.AbstractRelic.RelicTier;
+
+import PrismaticEvents.PrismaticEventsMod;
 
 public class NeowsFallenEvent extends AbstractImageEvent{
 
-    public static final String ID = "NeowsFallen";
+    public static final String ID = PrismaticEventsMod.makeID(NeowsFallenEvent.class.getSimpleName());
+    private static final EventStrings eventStrings = CardCrawlGame.languagePack.getEventString(ID);
+    
+    private static final String NAME = eventStrings.NAME;
+    public static final String[] DESCRIPTIONS = eventStrings.DESCRIPTIONS;
+    public static final String[] OPTIONS = eventStrings.OPTIONS;
+    public static final String IMG = makeEventPath(NeowsFallenEvent.class.getSimpleName() + ".png");
 
-    public NeowsFallenEvent(String title, String body, String imgUrl) {
-        super(title, body, imgUrl);
-        //TODO Auto-generated constructor stub
+    private int screenNum = 0;
+    private AbstractRelic startingRelic;
+    private AbstractRelic gift1;
+    private AbstractRelic gift2;
+    
+    
+    public NeowsFallenEvent() {
+        super(NAME, DESCRIPTIONS[0], IMG);
+        
+        ArrayList<AbstractRelic> playerStarterRelics = new ArrayList<>();
+        for (AbstractRelic r : AbstractDungeon.player.relics){
+            if (r.tier.equals(RelicTier.STARTER)){
+                playerStarterRelics.add(r);
+            }
+        }
+        this.startingRelic = playerStarterRelics.get(0);
+        ArrayList<AbstractRelic> relics = new ArrayList<>();
+
+        for (AbstractRelic r : RelicLibrary.starterList){
+            for (AbstractRelic r2 : playerStarterRelics){
+                if (!r.relicId.equals(r2.relicId)){
+                    relics.add(r);
+                }
+            }
+        }
+
+        this.gift1 = relics.get(AbstractDungeon.relicRng.random(relics.size() - 1));
+        relics.remove(this.gift1);
+        this.gift2 = relics.get(AbstractDungeon.relicRng.random(relics.size() - 1));
+
+        imageEventText.setDialogOption(OPTIONS[0] + startingRelic.name + OPTIONS[1] + this.gift1.name, this.gift1.makeCopy()); // Lose starting relic, gain starting relic
+        imageEventText.setDialogOption(OPTIONS[0] + startingRelic.name + OPTIONS[2]); // Lose starting relic, gain random starting relic
+        imageEventText.setDialogOption(OPTIONS[3]); // Leave
     }
 
     @Override
-    protected void buttonEffect(int arg0) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'buttonEffect'");
+    protected void buttonEffect(int i) {
+        switch (screenNum){
+            case 0: // Starting Screen
+                switch(i){
+                    case 0: //Lose starting relic, gain starting relic
+                        this.imageEventText.updateBodyText(DESCRIPTIONS[1]);
+                        this.imageEventText.updateDialogOption(0, OPTIONS[4]);
+                        this.imageEventText.clearRemainingOptions();
+                        screenNum = 1;
+
+                        AbstractDungeon.player.loseRelic(this.startingRelic.relicId);
+                        AbstractDungeon.getCurrRoom().spawnRelicAndObtain((float)(Settings.WIDTH / 2), (float)(Settings.HEIGHT / 2), this.gift1.makeCopy());
+                        
+                        return;
+                    case 1: //Lose starting relic, gain random starting elic
+                        this.imageEventText.updateBodyText(DESCRIPTIONS[2]);
+                        this.imageEventText.updateDialogOption(0, OPTIONS[4]);
+                        this.imageEventText.clearRemainingOptions();
+                        screenNum = 1;
+
+                        AbstractDungeon.player.loseRelic(this.startingRelic.relicId);
+                        AbstractDungeon.getCurrRoom().spawnRelicAndObtain((float)(Settings.WIDTH / 2), (float)(Settings.HEIGHT / 2), this.gift2.makeCopy());
+
+                        return;
+                    case 2: // Leave
+                        this.imageEventText.updateBodyText(DESCRIPTIONS[3]);
+                        this.imageEventText.updateDialogOption(0, OPTIONS[4]);
+                        this.imageEventText.clearRemainingOptions();
+                        screenNum = 1;
+                        
+                        return;
+                }
+            case 1: // Confirmation Screen
+                switch(i){
+                    case 0:
+                        openMap();
+                        return;
+                }
+                return;
+        }
     }
     
 }
